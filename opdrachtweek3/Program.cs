38737a51bd1c83711d2de9a467dcc1e6468e1642
quinleys <quinleys@student.arteveldehs.be>
2017-10-16 17:23:50 +0200

    first commit

---
+++ opdrachtweek3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace opdrachtweek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Geef een getal in: ");
+        int j = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("vermenigvuldigtal:{0}",j);
+
+        for (int i = 0; i < 10; i++) // startwaarde, voorwaarde en verhogen/verlagen teller
+            {
+                Console.WriteLine( "{0} * {1} = {2}",i,j,i*j);
+
+            }
+
+
+        }
+    }
+}
